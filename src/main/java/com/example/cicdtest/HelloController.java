ad37cc95ff786a48d489faba31814f706bc3489a
okonys <62321546+ysk1007@users.noreply.github.com>
2025-06-20 12:17:34 +0900

    commit 3

--- src/main/java/com/example/cicdtest/HelloController.java
+++ src/main/java/com/example/cicdtest/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 	@GetMapping("/")
 	public String hello() {
-		return "#2 hello";
+		return "#3 hello";
 	}
 }
